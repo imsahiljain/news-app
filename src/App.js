c186424721805fada5a5f4b1b3a6dcfbbaa4c2a0
imsahiljain <emailofsahiljain@gmail.com>
2022-01-24 19:47:48 +0530

    add: news comp.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import Navbar from "./components/Navbar";
+import News from './components/News';
 
 class App extends Component {
   render() {
     return (
       <>
         <Navbar />
+        <News />
       </>
     );
   }
 }
 
 export default App;
+
+// 280fa0816b344dae8da567443c8180b6
