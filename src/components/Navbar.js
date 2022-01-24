0b0cc00d90ab880e4a7cceafc9397539e886f512
imsahiljain <emailofsahiljain@gmail.com>
2022-01-24 16:49:49 +0530

    setup chakra-ui

---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+    Flex,
+    Spacer,
+    Box,
+    Button,
+    Heading,
+    useColorMode,
+    Link
+} from '@chakra-ui/react'
+import {
+    MoonIcon,
+    SunIcon
+} from '@chakra-ui/icons';
+import { IconButton } from '@chakra-ui/button'
+
+const Navbar = () => {
+    const { colorMode, toggleColorMode } = useColorMode()
+
+    return (
+        <>
+            
+        </>
+    );
+};
+
+export default Navbar
