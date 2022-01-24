6d6239fa4bf6322e6ebacb349cb4ccbaab043df1
imsahiljain <emailofsahiljain@gmail.com>
2022-01-24 17:11:14 +0530

    add: favicon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navbar from "./components/Navbar";
 
-function App() {
-  return (
-    <>
-     <Navbar />
-    </>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Navbar />
+      </>
+    );
+  }
 }
 
 export default App;
