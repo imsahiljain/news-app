63ab5d5cc550fbaff2ca276e9037bd73c9c43d54
imsahiljain <emailofsahiljain@gmail.com>
2022-01-24 22:52:43 +0530

    add: pagination

--- src/components/News.js
+++ src/components/News.js
@@ -1,34 +1,64 @@
 import React, { Component } from 'react';
 import {
     SimpleGrid,
-    Heading
+    Heading,
+    Button,
+    Container 
 } from '@chakra-ui/react'
 import NewsItem from './NewsItem';
 import "../assets/css/News.css";
-
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
 
 class News extends Component {
+    static defaultProps = {
+        category: 'general'
+    }
 
     constructor() {
         super();
         this.state = {
-            articles: []
+            articles: [],
+            page: 1
         }
     }
 
     async componentDidMount() {
-        const url = "https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=280fa0816b344dae8da567443c8180b6"
+        const url = `https://newsapi.org/v2/top-headlines?country=us&category=${this.props.category}&apiKey=280fa0816b344dae8da567443c8180b6&pageSize=21`
         const jsonData = await fetch(url)
         const parseData = await jsonData.json()
         this.setState({
             articles: parseData.articles
-        })
+        });
+    };
+
+    handlePreviousClick = async () => {
+        const url = `https://newsapi.org/v2/top-headlines?country=us&category=${this.props.category}&apiKey=280fa0816b344dae8da567443c8180b6&page=${this.state.page - 1}&pageSize=21`
+        const jsonData = await fetch(url)
+        const parseData = await jsonData.json()
+        this.setState({
+            page: this.state.page - 1,
+            articles: parseData.articles
+        });
     }
-    
+
+    handleNextClick = async () => {
+        const url = `https://newsapi.org/v2/top-headlines?country=us&category=${this.props.category}&apiKey=280fa0816b344dae8da567443c8180b6&page=${this.state.page + 1}&pageSize=21`
+        const jsonData = await fetch(url)
+        const parseData = await jsonData.json()
+        this.setState({
+            page: this.state.page + 1,
+            articles: parseData.articles
+        });
+    }
+
     render() {
         return (
             <>
-                <Heading className='newsHeading' as='h2' size='3xl' align='center' fontFamily="Inter" isTruncated>
+                <Heading pt='100' className='newsHeading' as='h2' size='3xl' align='center' fontFamily="Inter" isTruncated>
                     Latest News
                 </Heading>
 
@@ -39,6 +69,18 @@ class News extends Component {
                         )
                     })}
                 </SimpleGrid>
+
+                <Container display='flex' alignItems='center' justifyContent='center' pb='20'>
+                    <Button mr='5' disabled={this.state.page <= 1} colorScheme='blue' variant='solid' onClick={this.handlePreviousClick}>
+                        Previous
+                    </Button>
+
+                    <Button colorScheme='blue' variant='solid' onClick={this.handleNextClick}>
+                        Next
+                    </Button>
+                    
+                </Container>
+
 
             </>
         );
